ca09c8daac96fe628b6208db12fc6501e8d66a19
Nguyen Do <do.n@northeastern.edu>
2021-02-26 23:15:45 -0500

    Initial Commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by David on 2/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
